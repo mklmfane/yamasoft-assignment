ac3c098e0d28af4de0d1f291bcef652d12e2162a
mirceaconstantin434 <mirceaconstantin434@outlook.com>
2025-10-18 22:51:18 +0300

    Correct iam role script in github pipeline

--- modules/s3_bucket_state/output.tf
+++ modules/s3_bucket_state/output.tf
@@ -4,6 +4,7 @@ output "s3_bucket_id" {
   value       = aws_s3_bucket.tf_state.id
 }
 
+
 output "lock_table_name" {
   description = "Name of the DynamoDB state lock table (existing or created)."
   value       = var.lock_table != "" ? var.lock_table : try(aws_dynamodb_table.tf_locks[0].name, null)
