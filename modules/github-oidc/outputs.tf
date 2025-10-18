517b0506f93e40fb7db26745950523bae16c3ea2
mirceaconstantin434 <mirceaconstantin434@outlook.com>
2025-10-18 23:53:48 +0300

    REsolve policy duplication

--- modules/github-oidc/outputs.tf
+++ modules/github-oidc/outputs.tf
@@ -1,14 +1,13 @@
+
+# -----------------------------------------------------------------------------
+# Output the OIDC provider ARN and role ARN
+# -----------------------------------------------------------------------------
 output "oidc_provider_arn" {
-  description = "The ARN of the OIDC provider."
   value       = try(data.external.oidc_provider_probe.result.arn, null)
+  description = "The ARN of the OIDC provider."
 }
 
-output "oidc_role" {
+output "oidc_role_arn" {
+  value       = try(data.external.oidc_role_probe.result.arn, null)
   description = "The ARN of the OIDC role."
-  value       = try(data.external.oidc_role_probe.result.arn, null)
-}
-
-output "role_arn" {
-  description = "The ARN of the IAM role."
-  value       = try(data.external.oidc_role_probe.result.arn, null)
 }
