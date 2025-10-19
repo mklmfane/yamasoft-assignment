1b2711f855e9a1729620c26b42596570854fd776
mirceaconstantin434 <mirceaconstantin434@outlook.com>
2025-10-19 16:20:51 +0300

    Correct deploy job inside terraform workflow for github actions

--- modules/github-oidc/outputs.tf
+++ modules/github-oidc/outputs.tf
@@ -6,23 +6,11 @@ output "effective_oidc_provider_arn" {
   value       = local.federated_provider_arn
 }
 
-
-#output "effective_role_arn" {
-#  description = "IAM role ARN (created by module, provided via input, or discovered)."
-#  value = length(try(aws_iam_role.this[0].arn, "")) > 0 ? aws_iam_role.this[0].arn : (length(trimspace(var.existing_role_arn)) > 0 ? trimspace(var.existing_role_arn) : trimspace(try(data.external.oidc_role_probe.result.arn, "")))
-#}
-
+# Role ARN (created → input → probe → "")
 output "effective_role_arn" {
-  description = "IAM role ARN (created, provided, or discovered)."
-  value = length(try(aws_iam_role.this[0].arn, "")) > 0
-    ? aws_iam_role.this[0].arn
-    : (
-        length(trimspace(var.existing_role_arn)) > 0
-        ? trimspace(var.existing_role_arn)
-        : trimspace(try(data.external.oidc_role_probe.result.arn, ""))
-      )
+  description = "IAM role ARN (created by module, provided via input, or discovered)."
+  value = length(try(aws_iam_role.this[0].arn, "")) > 0 ? aws_iam_role.this[0].arn : (length(trimspace(var.existing_role_arn)) > 0 ? trimspace(var.existing_role_arn) : trimspace(try(data.external.oidc_role_probe.result.arn, "")))
 }
-
 
 # Optional: expose whether we attempted to create things (purely from inputs)
 output "create_provider" {
@@ -34,4 +22,3 @@ output "create_role" {
   description = "Whether the module was set (by inputs) to create the IAM role."
   value       = local.create_role_gate
 }
-
