072ff29f60c19d505430fd636c0afa084e75bd36
mirceaconstantin434 <mirceaconstantin434@outlook.com>
2025-10-19 19:29:02 +0300

    Test deploy related to migrating of local state file to the s3 bucket

--- output.tf
+++ output.tf
@@ -13,3 +13,15 @@ output "github_oidc_role_arn" {
   description = "IAM role ARN used by GitHub OIDC."
   value       = length(try(module.github_oidc.effective_role_arn, "")) > 0 ? module.github_oidc.effective_role_arn : ""
 }
+
+output "s3_bucket_id" { 
+  value = module.s3_bucket_state_oidc.s3_bucket_id 
+}
+
+output "backend_key" { 
+  value = module.s3_bucket_state_oidc.lock_table_name 
+}
+
+output "effective_role_arn" { 
+  value = module.github_oidc.effective_role_arn
+}
